492581bbcf22c282e158104042d33e717565132e
guille-gallo <guille.gallo88@gmail.com>
2024-08-22 16:20:13 +0200

    update Container height

--- src/components/Styled/Container/index.tsx
+++ src/components/Styled/Container/index.tsx
@@ -4,8 +4,8 @@ import styled from 'styled-components';
 const Container = styled.div`
     display: flex;
     flex-direction: column;
-    max-height: 80vh;
-    min-height: 80vh;
+    max-height: 30vh;
+    min-height: 30vh;
     width: 800px;
     overflow-y: scroll;
     margin: 20px auto;
